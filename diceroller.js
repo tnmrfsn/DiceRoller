68ed852dfd8b74394b45fdc79bb8a33cec27f19a
Md. Ikramul Islam <tnmrfsn@gmail.com>
2025-06-13 09:31:20 +0600

    bakground added

--- diceroller.js
+++ diceroller.js
@@ -1,11 +1,11 @@
 function rollDice() {
-  const numOfDice = document.getElementById("numOfDice").value;
+  let numOfDice = document.getElementById("numOfDice");
   const diceResult = document.getElementById("diceResult");
   const diceImages = document.getElementById("diceImages");
   const values = [];
   const images = [];
 
-  for (let i = 0; i < numOfDice; i++) {
+  for (let i = 0; i < numOfDice.value; i++) {
     const value = Math.floor(Math.random() * 6) + 1;
     values.push(value);
     images.push(
@@ -15,4 +15,7 @@ function rollDice() {
 
   diceResult.textContent = `dice: ${values.join(",")}`;
   diceImages.innerHTML = images.join("");
+
+  numOfDice.value = "1";
+  console.log(numOfDice);
 }
